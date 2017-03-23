30930b20276c5807a3143962dc92bc9ccc82b3d1
michaltaratuta <m.taratuta@hotmail.com>
2017-03-23 16:25:19 +0000

    Change stingray-redis-sentinel.py for working copy

--- stingray-redis-sentinel.py
+++ stingray-redis-sentinel.py
@@ -1,4 +1,6 @@
+#! /usr/bin/env python
 import redis
+import sys
 import socket
 from optparse import OptionParser
 
@@ -11,18 +13,29 @@ parser.add_option("--node", dest="node")
 parser.add_option("--port", dest="port")
 parser.add_option("--sentinel-witness", dest="sentinel_witness")
 parser.add_option("--sentinel-port", dest="sentinel_port")
+parser.add_option("--sentinel-monitor-name", dest="sentinel_monitor_name")
+parser.add_option("--stingray-pool-name", dest="stingray_pool_name")
+parser.add_option("--verbose", action="store_false", dest="verbose")
+parser.add_option("--failures_left", dest="failures_left")
+
+
     
 (options, args) = parser.parse_args()
 
-# connect to Redis
-sentinel_host = options.sentinel_witness
+# Healthcheck variables
+pool_node = options.node
+sentinel_witness = options.sentinel_witness
 sentinel_port = options.sentinel_port
-r = redis.StrictRedis(host='poc-witness-sentinel.int.mol.dmgt.net', port=26379)
-# r = redis.StrictRedis(host=ipaddr_from_stingray, port=26379)
+sentinel_monitor_name = options.sentinel_monitor_name
+stingray_pool_name = options.stingray_pool_name
 
-# Query Sentinel for the IP of the master for "monitor_name" group.
-def sentinel_check_master(monitor_name):
-    get_master_bytes = r.sentinel_get_master_addr_by_name(monitor_name)
+# Script variables
+node_list = [pool_node, sentinel_witness]
+active_master_list = []
+
+def sentinel_check_master(sentinel_monitor_name):
+    #connect_sentinel(host_name, port_name)
+    get_master_bytes = r.sentinel_get_master_addr_by_name(sentinel_monitor_name)
     
     list_master_bytes = list(get_master_bytes)
 
@@ -37,14 +50,15 @@ def sentinel_check_master(monitor_name):
 def get_host_name(master_node):
     
     get_host_by_addr = socket.gethostbyaddr(master_node[0])
-    print (get_host_by_addr[0])
     return get_host_by_addr[0]
-    
-    
-#get_arguments()    
-get_host_name(sentinel_check_master('poc-a1'))
-print (options.ipaddr)
-print (options.node)
-print (options.port)
-print (options.sentinel_witness)
-print (options.sentinel_port)
+
+for node in node_list:
+    # connect to Sentinel
+    r = redis.StrictRedis(host=node, port=sentinel_port)
+    # Get the hostname of current master
+    active_master_list.append(get_host_name(sentinel_check_master(sentinel_monitor_name)))
+
+if active_master_list[0] == active_master_list[1] and active_master_list[0] == pool_node:
+    sys.exit(0)
+else:
+    sys.exit(1)
